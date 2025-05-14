8a3a566a78adc1b0ee7d92fadeee2a65e5fb7a1a
Keijiro Takahashi <keijiro@gmail.com>
2025-05-14 11:54:39 +0900

    Better UI for TimingTest

--- Assets/Scripts/TimingTest.cs
+++ Assets/Scripts/TimingTest.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
+using System.Collections.Generic;
 
 public sealed class TimingTest : MonoBehaviour
 {
     [SerializeField] InputAction _action = null;
+
+    Queue<string> _logLines = new Queue<string>();
+
+    string LogText => string.Join("\n", _logLines);
+
+    void AddLog(string line)
+    {
+        _logLines.Enqueue(line);
+        while (_logLines.Count > 40) _logLines.Dequeue();
+    }
 
     double _prevTime;
 
@@ -15,13 +26,17 @@ public sealed class TimingTest : MonoBehaviour
     {
         _action.performed += OnPerformed;
         _action.Enable();
+        AddLog("Waiting for input...");
     }
 
     void OnPerformed(InputAction.CallbackContext ctx)
     {
         var delta = ctx.time - _prevTime;
         var bpm = 60 / delta / 4;
-        UILabel.text = $"Calculated BPM: {bpm:.00}";
+        AddLog($"Interval: {delta * 1000:.00} ms / Calculated BPM: {bpm:.00}");
         _prevTime = ctx.time;
     }
+
+    void Update()
+      => UILabel.text = LogText;
 }
